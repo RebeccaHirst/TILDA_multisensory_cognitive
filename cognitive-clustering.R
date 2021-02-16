3ae30269a0a25aa089fa7fe5ce59902d8bcb1749
RebeccaHirst <rj.hirst@hotmail.co.uk>
2021-02-16 14:19:54 +0000

    conditional cluster selection
    Add argument to select which algorithm we are extracting clusters for

--- cognitive-clustering.R
+++ cognitive-clustering.R
@@ -8,6 +8,7 @@ library(cluster)
 library(GGally)
 library(plotly)
 library(clValid)# for cluster validation
+library(gridExtra)# for plotting grid plots
 
 #### functions ####
 
@@ -22,8 +23,10 @@ wssplot <- function(data, nc=15, seed=123){
        ylab="Sum of squares within a group")}
 
 #### prepare dataframe ####
+
 # thisDF = a dataframe containing one column with tilda_Serial and the remaining the cognitive measure recorded across n waves
 thisDf = animal_naming_df
+
 # set the serial number as the row names
 thisDf2 <- thisDf[,-1]
 rownames(thisDf2) <- thisDf[,1]
@@ -33,9 +36,10 @@ df <-scale(thisDf2)
 df2<-as.data.frame(scale(thisDf2))
 df<-df2
 
-# check if there are any NA of NaN values in the dataframe (clValid will not know how to cluster)
+# check for NA or NaN values in the dataframe (clValid will not know how to cluster)
 sum(is.nan(df))
 sum(is.na(df))
+# keep only complete cases
 df <- df[complete.cases(df), ]
 
 #### Run clustering ####
@@ -43,10 +47,12 @@ df <- df[complete.cases(df), ]
 # set seed to allow replication
 set.seed(123)
 
-# run clustering across a range of K and algorithms to identify optimal clustering approach\
+# run clustering across a range of K and algorithms to identify optimal clustering approach for this measure
 
-#internal validity: Connectvity, Dunns Index, Sillhoette
+# internal validity: Connectvity (lower better), Dunns Index (higher better), Sillhoette (higher better)
 intern<- clValid(df, 2:6, clMethods=c("kmeans", "pam", "hierarchical"), validation = "internal")
+
+# summarise the results of clValid
 summary(intern)
 
 # plot the measures of internal validity to compare models
@@ -60,24 +66,38 @@ par(op)
 
 #### Evaluate optimal clustering ####
 optimal_k <- 3
+optimal_algorithm <- "hierarchical"
 
-#### Hierarchical clustering ####
-
-# plot the cluster dendogram (if hierarchical clustering outperforms other algorithms)
-hc <- clusters(intern, "hierarchical")
-# cut the tree at the optimal n clusters
-cut_avg <- cutree(hc, k = optimal_k )
-
-# color the branches so that we can see the individual clusters
-suppressPackageStartupMessages(library(dendextend))
-avg_dend_obj <- as.dendrogram(hc)
-avg_col_dend <- color_branches(avg_dend_obj, k = optimal_k )
-plot(avg_col_dend)
-
-# Append the cluster results back onto the original data frame for interpretation 
-suppressPackageStartupMessages(library(dplyr))
-df_cl <- mutate(df, cluster = cut_avg)
-count(df_cl$cluster)
+if (optimal_algorithm=="hierarchical"){
+  #### Plot and evaluate hierarchical clustering ####
+  print("selected algorithm was hierarchical")
+  
+  # extract the hierarchical cluster object from intern
+  hc <- clusters(intern, "hierarchical")
+  
+  # cut the tree at the optimal n clusters
+  cut_avg <- cutree(hc, k = optimal_k )
+  
+  # color the branches so that we can see the individual clusters
+  suppressPackageStartupMessages(library(dendextend))
+  avg_dend_obj <- as.dendrogram(hc)
+  avg_col_dend <- color_branches(avg_dend_obj, k = optimal_k )
+  plot(avg_col_dend)
+  
+  # Append the cluster results back onto the original data frame for interpretation 
+  suppressPackageStartupMessages(library(dplyr))
+  df_cl <- mutate(df, cluster = cut_avg)
+  count(df_cl$cluster)
+  
+}
+if (optimal_algorithm=="k-means"){
+  #### Plot and evaluate k-means clustering ####
+  k_df<-df
+  k_df$cluster <- as.factor(intern@clusterObjs$kmeans[[optimal_k]]$cluster)
+  fviz_cluster(intern@clusterObjs$kmeans[[optimal_k]], df, ellipse.type = "euclid")
+  
+  df_cl<-k_df
+}
 
 
 
@@ -88,15 +108,14 @@ gd <- df_cl %>%
   group_by(cluster) %>% 
   summarise(
     immediaterecall_total_W1 = mean(immediaterecall_total_W1),
-    immediaterecall_total_W2= mean(immediaterecall_total_W2),
-    immediaterecall_total_W3= mean(immediaterecall_total_W3),
-    immediaterecall_total_W4= mean(immediaterecall_total_W4),
-    immediaterecall_total_W5= mean(immediaterecall_total_W5),
+    immediaterecall_total_W2 = mean(immediaterecall_total_W2),
+    immediaterecall_total_W3 = mean(immediaterecall_total_W3),
+    immediaterecall_total_W4 = mean(immediaterecall_total_W4),
+    immediaterecall_total_W5 = mean(immediaterecall_total_W5),
   )
 
 
 
-library(gridExtra)
 p1 <- ggplot(df_cl, aes(x = cluster, y = immediaterecall_total_W1, fill = paste(cluster))) +
   geom_point()+
   geom_bar(data = gd, stat = "identity", alpha = .3)
@@ -121,20 +140,10 @@ p5 <- ggplot(df_cl, aes(x = cluster, y = immediaterecall_total_W5, fill = paste(
 
 pp<-grid.arrange(p1, p2,p3,p4, p5, nrow = 2, widths=c(2, 2, 2))
 
-hc_df<-df_cl
-# note: would access the cluster labels for kmeans or PAM using intern@clusterObjs$kmeans$`2` (for 2 clusters)
-# hierarchical clustering method uses hclust https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/hclust
-
 # stability validation 
 
 stab <- clValid(df, 2:6, clMethods=c("kmeans","pam", "hierarchical"),
                 validation="stability")
 
-#### Plot and evaluate k-means clustering ####
-k_df<-df
-k_df$cluster <- as.factor(intern@clusterObjs$kmeans$`2`$cluster)
-fviz_cluster(intern@clusterObjs$kmeans$`2`, df, ellipse.type = "euclid")
-
-df_cl<-k_df
 
 
