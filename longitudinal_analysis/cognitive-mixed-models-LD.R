76b8c8abbd916bd82f151aa6d2b4591cd7103f4b
RebeccaHirst <rj.hirst@hotmail.co.uk>
2021-12-14 13:18:06 +0000

    update ld figs

--- longitudinal_analysis/cognitive-mixed-models-LD.R
+++ longitudinal_analysis/cognitive-mixed-models-LD.R
@@ -10,7 +10,7 @@ delayed_recall_groups, animal_naming_groups and immediate_recall_groups respecti
 "
 
 # set to "animal naming", "delayed recall" or "immediate recall"
-this_cog_measure <- "animal naming"
+this_cog_measure <- "immediate recall"
 
 #If plotting only we will only run full models and plot them, likelihood ratio tests willnot be performed
 plotting_only <- FALSE
